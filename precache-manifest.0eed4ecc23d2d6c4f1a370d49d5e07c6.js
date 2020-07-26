c16a988e7496c7f22ea39a5862da499615348753
Zack McKenna <zackmckenna@gmail.com>
2020-07-26 19:19:54 -0400

    Updates

---
+++ precache-manifest.0eed4ecc23d2d6c4f1a370d49d5e07c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c123e84229655aa88bf551b170b137",
+    "url": "/tutorial-27/index.html"
+  },
+  {
+    "revision": "d12fe67c4a0a90d21a90",
+    "url": "/tutorial-27/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "78688c9f9fd9de6064f9",
+    "url": "/tutorial-27/static/js/2.369386f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tutorial-27/static/js/2.369386f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12fe67c4a0a90d21a90",
+    "url": "/tutorial-27/static/js/main.c9288bea.chunk.js"
+  },
+  {
+    "revision": "39080c9e7da8c82c8bd5",
+    "url": "/tutorial-27/static/js/runtime-main.4edc1abd.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/tutorial-27/static/media/mintbean.cf18b629.png"
+  }
+]);
