5e9653a80a4c50534d3ce9672f53d9befc158a51
Zack McKenna <zackmckenna@gmail.com>
2020-07-27 15:45:43 -0400

    added picture download feature

--- src/components/CameraFeed.jsx
+++ src/components/CameraFeed.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { Button } from 'react-bootstrap'
+import { Button, Col, Row } from 'react-bootstrap'
 
 const CameraFeed = () => {
   const videoPlayer = useRef(null)
@@ -16,19 +16,19 @@ const CameraFeed = () => {
   }, [])
 
   const processDevices = (devices) => {
-        devices.forEach(device => {
-          console.log('Device:', device.label)
-            setDevice(device)
-        })
-    }
+    devices.forEach(device => {
+      console.log('Device:', device.label)
+      setDevice(device)
+    })
+  }
 
   const setDevice = async (device) => {
-      const { deviceId } = device
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: { deviceId } })
+    const { deviceId } = device
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: { deviceId } })
       .then(stream => videoPlayer.current.srcObject = stream)
       .catch(err => console.log(err))
-      videoPlayer.current.play()
-    }
+    videoPlayer.current.play()
+  }
 
   const turnCameraOff = () => {
     videoPlayer.current.srcObject.getVideoTracks().forEach(track => track.stop())
@@ -40,19 +40,42 @@ const CameraFeed = () => {
   }
 
   const takePhoto = () => {
-      const context = canvas.current.getContext('2d')
-      context.drawImage(videoPlayer.current, 0, 0, canvas.current.width, canvas.current.height)
+    const context = canvas.current.getContext('2d')
+    context.drawImage(videoPlayer.current, 0, 0, canvas.current.width, canvas.current.height)
   }
 
 
+  const savePhoto = () => {
+    canvas.current.toBlob(
+      blob => {
+        const anchor = document.createElement('a')
+        anchor.download = 'my-file-name.jpg' // optional, but you can give the file a name
+        anchor.href = URL.createObjectURL(blob)
+        anchor.click(); // ✨ magic!
+        URL.revokeObjectURL(anchor.href) // remove it from memory and save on memory! 😎
+      },
+      'image/jpeg',
+      0.9,
+    )
+  }
+
   return (
     <div className="c-camera-feed">
-      <div className="c-camera-feed__viewer">
-        <video ref={videoPlayer} width="680" heigh="360" />
-      </div>
-      <Button className='mr-2' onClick={() => turnCameraOff()}>Turn off Camera</Button>
-      <Button className='mr-2' onClick={() => turnCameraOn()}>Turn on Camera</Button>
-      <Button onClick={() => takePhoto()}>Take photo!</Button>
+      <Row>
+        <Col>
+          <div className="c-camera-feed__viewer">
+            <video style={{ borderRadius: '3px' }} ref={videoPlayer} width="680" heigh="360" />
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button className='btn-warning rounded mb-2 ' onClick={() => takePhoto()}>Take photo!</Button>
+        </Col>
+      </Row>
+      <Button className='btn-danger mr-2' onClick={() => turnCameraOff()}>Turn off Camera</Button>
+      <Button className='btn-success mr-2' onClick={() => turnCameraOn()}>Turn on Camera</Button>
+      <Button onClick={(e) => savePhoto(e)}>Save photo!</Button>
       <div className="c-camera-feed__stage mt-2">
         <canvas className='canvas' width="680" height="360" ref={canvas} />
       </div>
